2927132cc4710cfb7ea77762d48b48b5f8e9d269
Yang-Kinz <57697988+kin-yansu@users.noreply.github.com>
2023-06-25 17:53:21 +0900

    Finish 3-2

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,35 @@
 // import { RouterLink, RouterView } from 'vue-router'
 import {ref} from 'vue'
 
-const name = ref('田中太郎')
+// 現在の日時
+const now = new Date()
+
+// 現在の日時の文字列を取得
+const nowStr = now.toLocaleString()
+
+// 現在の時刻文字列をテンプレート変数として用意
+let timeStr = nowStr
+
+// 同じく現在の時刻文字列をテンプレート変数としてref()で用意
+const timeStrRef = ref(nowStr)
+
+// 新しい時刻に変更する関数
+function changeTime(): void {
+  // 現在の日時を取得
+  const newTime = new Date()
+  // 現在の日時の文字列を取得
+  const newTimeStr = newTime.toLocaleString()
+  // 現在の時刻文字列をテンプレート変数に代入
+  timeStr = newTimeStr
+  // 同じく現在の時刻文字列をテンプレート変数としてref()で用意
+  timeStrRef.value = newTimeStr
+}
+
+setInterval(changeTime, 1000)
 </script>
 <template>
-  <h1>こんにちは{{ name }}</h1>
+  <p>現在時刻： {{ timeStr }}</p>
+  <p>現在時刻(ref)： {{ timeStrRef }}</p>
 </template>
 
 <style scoped>
