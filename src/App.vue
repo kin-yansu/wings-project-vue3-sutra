991fe4416fbc3c729262050ede5c6299fc5f5282
Yang-Kinz <57697988+kin-yansu@users.noreply.github.com>
2023-06-25 17:31:13 +0900

    Finish 3-1

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,11 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+// import { RouterLink, RouterView } from 'vue-router'
+import {ref} from 'vue'
+
+const name = ref('田中太郎')
 </script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <h1>こんにちは{{ name }}</h1>
 </template>
 
 <style scoped>
